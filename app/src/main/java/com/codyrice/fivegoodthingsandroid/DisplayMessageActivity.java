3551781ed0611dfbb45a88b456230154eaf5e6f1
Devin Cody Rice <cody.rice@gmail.com>
2017-11-21 16:08:39 -0800

    Five good things activity page

--- app/src/main/java/com/codyrice/fivegoodthingsandroid/DisplayMessageActivity.java
+++ app/src/main/java/com/codyrice/fivegoodthingsandroid/DisplayMessageActivity.java
@@ -12,13 +12,13 @@ public class DisplayMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String feeling = ((Integer) intent.getIntExtra(MainActivity.EXTRA_FEELING, 0)).toString();
-
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(feeling);
+//        // Get the Intent that started this activity and extract the string
+//        Intent intent = getIntent();
+//        String feeling = ((Integer) intent.getIntExtra(MainActivity.EXTRA_FEELING, 0)).toString();
+//
+//        // Capture the layout's TextView and set the string as its text
+//        TextView textView = findViewById(R.id.textView);
+//        textView.setText(feeling);
 
     }
 
