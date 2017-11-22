fc617906b8044ccedb84708ffe668688e98b3e9a
Devin Cody Rice <cody.rice@gmail.com>
2017-11-22 10:05:43 -0800

    Testing timed notifications

--- app/src/main/java/com/codyrice/fivegoodthingsandroid/DisplayMessageActivity.java
+++ app/src/main/java/com/codyrice/fivegoodthingsandroid/DisplayMessageActivity.java
@@ -1,10 +1,9 @@
 package com.codyrice.fivegoodthingsandroid;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
--- app/src/main/java/com/codyrice/fivegoodthingsandroid/MainActivity.java
+++ app/src/main/java/com/codyrice/fivegoodthingsandroid/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,4 +25,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
 }
