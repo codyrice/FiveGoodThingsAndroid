aa702e0c879cf67265f8eabff305c75cd0534acc
Devin Cody Rice <cody.rice@gmail.com>
2017-11-21 16:20:49 -0800

    Adding DoneActivity class and layout

--- app/src/main/java/com/codyrice/fivegoodthingsandroid/DisplayMessageActivity.java
+++ app/src/main/java/com/codyrice/fivegoodthingsandroid/DisplayMessageActivity.java
@@ -3,6 +3,8 @@ package com.codyrice.fivegoodthingsandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -12,14 +14,20 @@ public class DisplayMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-//        // Get the Intent that started this activity and extract the string
-//        Intent intent = getIntent();
-//        String feeling = ((Integer) intent.getIntExtra(MainActivity.EXTRA_FEELING, 0)).toString();
-//
-//        // Capture the layout's TextView and set the string as its text
-//        TextView textView = findViewById(R.id.textView);
-//        textView.setText(feeling);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String feeling = ((Integer) intent.getIntExtra(MainActivity.EXTRA_FEELING, 0)).toString();
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(feeling);
 
     }
 
+    /** Called when the user taps the Send button */
+    public void saveFeelings(View view) {
+        Intent intent = new Intent(this, DoneActivity.class);
+        startActivity(intent);
+
+    }
 }
